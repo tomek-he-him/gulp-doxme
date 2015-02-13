e4e2531b910642800f872343db8e99c8023d285f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-13 23:07:34 +0100

    Add a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/*jshint esnext: false, node: true, globalstrict: true */
+"use strict";
+
+var gulp = require("gulp");
+var to5 = require("gulp-6to5");
+
+
+var settings =
+  { scripts:
+    { source: "source/**/*.js"
+    , target:
+      { es5: "dist.es5"
+      , es6: "dist.es6"
+      }
+    }
+  };
+
+
+// `gulp build`
+// ------------
+
+gulp.task("build", ["scripts"]);
+
+
+// `gulp scripts`
+// --------------
+
+gulp.task("scripts", ["scripts:es6", "scripts:es5"]);
+
+gulp.task("scripts:es6", function () {
+  return gulp.src(settings.scripts.source)
+    .pipe(gulp.dest(settings.scripts.target.es6))
+    ;
+});
+
+gulp.task("scripts:es5", function () {
+  return gulp.src(settings.scripts.source)
+    .pipe(to5())
+    .pipe(gulp.dest(settings.scripts.target.es5))
+    ;
+});
