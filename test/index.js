608ff64598cf6a52ffd234a8e4795812232951ce
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-18 22:33:29 +0100

    Sketch out tests

--- test/index.js
+++ test/index.js
@@ -1,1 +1,22 @@
-var test = require("tape-catch");
+import test from "tape-catch";
+
+
+test("Processes JSON output of gulp-dox", (is) => {
+  is.skip("outputting a markdown file");
+  is.skip("with the same content as doxme");
+  is.end();
+  });
+
+test("Processes file.data", (is) => {
+  is.skip("outputting a markdown file");
+  is.skip("with the same content as doxme");
+  is.end();
+  });
+
+test("Emits errors", (is) => {
+  is.skip("when given an invalid file");
+  is.skip("when given a broken JSON file");
+  is.skip("when given broken file.data");
+  is.end();
+  });
+
