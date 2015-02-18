550157bada593d9f61461b4eb647d26f3e329ab9
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-02-18 23:22:15 +0100

    Test behavior with file.data

--- test/index.js
+++ test/index.js
@@ -1,22 +1,65 @@
-import test from "tape-catch";
+import fs from "fs";
+import path from "path";
 
+import dox from "dox";
+import originalDoxme from "doxme";
+import test from "tape-catch";
+import spigot from "stream-spigot";
+import File from "vinyl";
+
+import doxme from "../source/gulp-doxme";
+
+
+var doxOutput = dox.parseComments(
+  fs.readFileSync(path.resolve(__dirname, "fixtures/input.js")).toString()
+  );
+var doxOutputFile = new File(
+  { cwd: "/"
+  , base: "/base/"
+  , path: "/base/anything.json"
+  , contents: new Buffer(JSON.stringify(doxOutput))
+  });
+
+
+test("Processes file.data", (is) => {
+  is.plan(3);
+
+  var inputJSON = "[]";
+  var inputFile = doxOutputFile.clone();
+  inputFile.contents = new Buffer(inputJSON);
+  inputFile.data = doxOutput;
+
+  spigot({objectMode: true}, [inputFile])
+    .pipe(doxme())
+    .on("data", (outputFile) => {
+      var output = outputFile.contents.toString();
+
+      is.notEqual
+        ( output
+        , doxme(inputJSON)
+        , "preferring file.data over JSON"
+        );
+      is.equal
+        ( path.extname(outputFile.path)
+        , ".md"
+        , "outputting a markdown file"
+        );
+      is.equal
+        ( output
+        , originalDoxme(doxOutput)
+        , "with the same content as doxme's output"
+        );
+      });
+  });
 
 test("Processes JSON output of gulp-dox", (is) => {
   is.skip("outputting a markdown file");
-  is.skip("with the same content as doxme");
-  is.end();
-  });
-
-test("Processes file.data", (is) => {
-  is.skip("outputting a markdown file");
-  is.skip("with the same content as doxme");
+  is.skip("with the same content as doxme's output");
   is.end();
   });
 
 test("Emits errors", (is) => {
-  is.skip("when given an invalid file");
-  is.skip("when given a broken JSON file");
   is.skip("when given broken file.data");
+  is.skip("when given an invalid JSON file");
   is.end();
   });
-
